51c9cf8a0eb435f1b4b0453b44c4b780689849ca
anosikeosifo <appdev.anosikeosifo@gmail.com>
2015-04-26 06:50:38 +0100

    add token authentication

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,11 +7,27 @@ RSpec.describe User, type: :model do
   it { should respond_to(:email) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:auth_token) }
 
   it { should validate_presence_of(:email) }
   it { should validate_uniqueness_of(:email) }
   it { should validate_presence_of(:password) } 
   it { should validate_confirmation_of(:password) }
 
-  it { should be_valid }
+  it { should validate_uniqueness_of(:auth_token) }
+
+  describe "#generate_auth_token!" do
+    before(:each) { @user = FactoryGirl.create :user }
+    it "generates auth_tokens dynamically" do
+      Devise.stub(:friendly_token).and_return("uniquetoken123")
+      @user.generate_auth_token!
+      expect(@user.auth_token).to eql("uniquetoken123")
+    end
+
+    it "generates a new token if the previously generated has alreeady been taken" do
+      existing_user = FactoryGirl.create(:user, auth_token: "user_tokenX")
+      @user.generate_auth_token!
+      expect(@user.auth_token).not_to eql(existing_user.auth_token)  
+    end
+  end
 end
