b67bbf9d2329039318d83b69dec5e42239ef985c
anosikeosifo <appdev.anosikeosifo@gmail.com>
2015-05-23 14:52:31 +0100

    Add image upload from feature for posts

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -50,6 +50,45 @@ class Api::V1::PostsController < ApplicationController
 
   private 
     def post_params
-      params.require(:post).permit(:description, :image_url, :user_id, :removed)
+      # params.require(:post).permit(:description, :image_url, :user_id, :removed, :image_url, :image_data)
+      json_data = JSON.parse(request.raw_post)
+      params = ActionController::Parameters.new(json_data)
+
+      params = params.require(:post).permit(:description, :image_url, :user_id, :removed, :image_url, :image_data)
+      
+      params[:image_url] = decode_image_data(params[:image_data])
+    end
+
+    def decode_image_data(base64_image_data)
+      filename = "post_photo"
+      in_content_type, encoding, string = base64_image_data.split(/[:;,]/)[1..3]
+      # data = PostUploader.new(Base64.decode64(base64_image_data))
+
+      #create a new tempfile to hold the uploaded photo
+      @tempFile = TempFile.new(filename)
+      @tempFile.binmode
+      @tempfile.write Base64.decode64(string)
+      @tempfile.rewind
+
+      #for security, i want to use the actual content type, not just what was passed in.
+      content_type = `file --mime -b #{@tempfile.path}`.split(";")[0]
+
+      #i check that an appropriate content_type is passed in, then assign it to the image
+      extension = content_type.match(/gif|jpg|jpeg|png/).to_s
+      filename += ".#{extension}" if extension #appebd the appropriate extension
+    
+      ActionDispatch::Http::UploadedFile.new({
+        tempfile: @tempfile,
+        content_type: content_type,
+        filename: filename
+      })
+    end
+
+
+    def clean_tempfile
+      if @tempFile
+        @tempfile.close
+        @tempFile.unlink
+      end
     end
 end
