375adc9cced77c5b7506bc16f8f0910234c9d3ff
anosikeosifo <appdev.anosikeosifo@gmail.com>
2015-05-23 16:46:35 +0100

    Add created_at to post serializer

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,5 +1,5 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :description, :image_url, :removed
+  attributes :id, :description, :image_url, :removed, :created_at
   has_one :user #this embeds the related user to the post json reponse
   has_many :comments
 end
