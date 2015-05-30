d531b174cb9ff1e6140a5d33ec40b24d20ca33f4
anosikeosifo <appdev.anosikeosifo@gmail.com>
2015-05-30 21:00:42 +0100

    Re-update controller to bypass CSRF token validation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
-  skip_before_action :verify_authenticity_token, if: :json_request?
+  # skip_before_action :verify_authenticity_token, if: :json_request?
   include Authenticable
+
+  protected
+
+  def json_request?
+    request.format.json?
+  end
 end
