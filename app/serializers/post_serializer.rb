1b065663c4537654ff04bc9508f140f4c93e5dc2
anosikeosifo <appdev.anosikeosifo@gmail.com>
2015-09-18 02:57:43 +0100

    Fix time zone issue

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,4 +1,5 @@
 class PostSerializer < ActiveModel::Serializer
+  ActiveSupport::JSON::Encoding.time_precision = 0
   attributes :id, :description, :image_url, :removed, :created_at
   has_one :user #this embeds the related user to the post json reponse
   has_many :comments
