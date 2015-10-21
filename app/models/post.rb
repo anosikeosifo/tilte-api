89cf27529ba0f8571ddc398871e48dc89a61e13d
anosikeosifo <appdev.anosikeosifo@gmail.com>
2015-10-21 18:09:29 +0100

    Fix bug with check_favorite feature

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,10 +2,10 @@ class Post < ActiveRecord::Base
   mount_base64_uploader :image, PostUploader
   mount_uploader :image, PostUploader
 
-  attr_accessor :is_favorite
+  attr_accessor :is_favorite, :is_favorite_request
   class_attribute :signed_in_user
 
-  after_initialize :check_favorite
+  after_initialize :check_favorite, if: :signed_in_user
 
   scope :for_user_feed, ->(user) { where(:user_id => user.following.pluck(:id).push(user.id)) }
   scope :by_user, ->(user) { where(user_id: user.id) }
@@ -23,7 +23,12 @@ class Post < ActiveRecord::Base
     self.increment!(:like_count, 1)
   end
 
-  def check_favorite
-    @is_favorite = signed_in_user.favorites.include?(self)
-  end
+  private
+    def check_favorite
+      @is_favorite = signed_in_user.favorites.include?(self)
+    end
+
+    # def is_favorite_request?
+    #   @is_favorite_request
+    # end
 end
