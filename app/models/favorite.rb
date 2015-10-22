71571bfc258c07e4ac8434c636b7ed166664afc4
anosikeosifo <appdev.anosikeosifo@gmail.com>
2015-10-22 03:48:34 +0100

    Add uniqueness validation for favorite feature

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,6 @@
 class Favorite < ActiveRecord::Base
   belongs_to :post
   belongs_to :user, counter_cache: true
+
+  validates :user, uniqueness: { scope: :post, message: "You already favorited this post" }
 end
