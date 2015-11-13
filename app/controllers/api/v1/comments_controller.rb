74f958ccf0c84d68ef368a6d2632103d4c309a8b
anosikeosifo <appdev.anosikeosifo@gmail.com>
2015-11-13 05:12:10 +0100

    Update strong paramaters for comments

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -45,6 +45,6 @@ class Api::V1::CommentsController < ApplicationController
 
   private
     def comment_params
-      params.require(:comment).permit(:text, :removed, :flagged, :user_id, :post_id )
+      params.require(:comment).permit(:text, :user_id, :post_id )
     end
 end
