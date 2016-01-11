c1b42505a7425d7b5167bca2572753138fe42648
anosikeosifo <appdev.anosikeosifo@gmail.com>
2016-01-11 14:53:53 +0100

    Change response status for unfollow header

--- app/controllers/api/v1/relationships_controller.rb
+++ app/controllers/api/v1/relationships_controller.rb
@@ -14,7 +14,7 @@ class Api::V1::RelationshipsController < ApplicationController
       other_user = User.find_by(id: params[:followed_id]) #Relationship.find_by(id: params[:id]).followed #gets the associated followed user
       user = User.find_by(id: params[:follower_id])
       user.unfollow!(other_user)
-      render json: { success: true, data: "", message: "Relationship destroyed" }, status: 204
+      render json: { success: true, data: "", message: "Relationship destroyed" }, status: 200
     rescue StandardError => e
       render json: { success: false, data: "", message: e.message }, status: 400
     end
