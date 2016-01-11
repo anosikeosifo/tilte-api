550021e198d98a85e5ea5522b290b6302c5552d7
anosikeosifo <appdev.anosikeosifo@gmail.com>
2016-01-11 14:07:35 +0100

    Add error-handling for unfollow logic

--- app/controllers/api/v1/relationships_controller.rb
+++ app/controllers/api/v1/relationships_controller.rb
@@ -10,9 +10,13 @@ class Api::V1::RelationshipsController < ApplicationController
   end
 
   def destroy
-    other_user = User.find_by(id: params[:followed_id]) #Relationship.find_by(id: params[:id]).followed #gets the associated followed user
-    user = User.find_by(id: params[:follower_id])
-    user.unfollow!(other_user)
-    head 204
+    begin
+      other_user = User.find_by(id: params[:followed_id]) #Relationship.find_by(id: params[:id]).followed #gets the associated followed user
+      user = User.find_by(id: params[:follower_id])
+      user.unfollow!(other_user)
+      render json: { success: true, data: "", message: "Relationship destroyed" }, status: 204
+    rescue StandardError => e
+      render json: { success: false, data: "", message: e.message }, status: 400
+    end
   end
 end
