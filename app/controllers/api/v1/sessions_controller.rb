80c1e129ea20fb0e9d2b4809f029149472571c01
anosikeosifo <appdev.anosikeosifo@gmail.com>
2016-01-29 09:21:26 +0100

    Modify session-creation logic

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,28 +1,24 @@
 class Api::V1::SessionsController < ApplicationController
   respond_to :json
   def create
-    user_email = params[:session][:email]
-    user_password = params[:session][:password]
-
+    user_email = params[:email]
+    user_password = params[:password]
     user = user_email.present? && User.find_by(email: user_email)
-
-
 
     if user.valid_password? user_password
       sign_in user, store: false
       user.generate_auth_token!
       user.save
-
-      render json: user, status: 200, location: [:api, user]
+      render json: { success: true, data: ActiveModel::ArraySerializer.new([user]), message: "" }, location: [:api, user], status: 200
     else
-      render json: { errors: user.errors }, status: 422
+      render json: { success: false, data: "", message: user.errors.full_messages.to_sentence }, status: 422, location: [:api, user]
     end
-  end 
+  end
 
   def destroy
     user = User.find_by(auth_token: params[:id])
     user.generate_auth_token! #here i destroy the user's current session by generating a new token, so the token on the client and in d db are diff
     user.save
-    head 204 
+    head 204
   end
 end
