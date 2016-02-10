71eebf491f21b383212205f9459bcf245375c606
anosikeosifo <appdev.anosikeosifo@gmail.com>
2016-02-10 09:48:47 +0100

    Add profile picture param in omniauth_sessions

--- app/controllers/api/v1/omniauth_sessions_controller.rb
+++ app/controllers/api/v1/omniauth_sessions_controller.rb
@@ -4,6 +4,7 @@ class Api::V1::OmniauthSessionsController < ApplicationController
     name = params[:name]
     uid = params[:uid]
     token = params[:token]
+    , avatar_url =  params[:profile_photo_url]
     provider = params[:provider] || "facebook"
     user = email.present? && User.joins(:identities).where(email: email, identities: { uid: uid }).first
 
@@ -13,7 +14,7 @@ class Api::V1::OmniauthSessionsController < ApplicationController
       user.save
       render json: { success: true, data: ActiveModel::ArraySerializer.new([user]), message: "existing_user" }, location: [:api, user], status: 200
     else
-      if create_user_from_oauth(email, name, "password", token, provider, uid)
+      if create_user_from_oauth(email, name, "password", token, provider, uid, avatar_url)
         render json: { success: true, data: ActiveModel::ArraySerializer.new([User.last]), message: "new_user" }, status: 200
       else
         render json: { success: false, data: [], message: "Login failed. Please try again." }, status: 200
@@ -23,8 +24,8 @@ class Api::V1::OmniauthSessionsController < ApplicationController
 
   private
 
-  def create_user_from_oauth(email, name, password, token, oauth_provider, uid)
-    new_user = User.create!(email: email, fullname: name, password: password)
+  def create_user_from_oauth(email, name, password, token, oauth_provider, uid, avatar_url)
+    new_user = User.create!(email: email, fullname: name, password: password, avatar: avatar_url)
     if new_user.persisted?
       new_user.identities.create(provider: oauth_provider, token: token, uid: uid)
       return true
