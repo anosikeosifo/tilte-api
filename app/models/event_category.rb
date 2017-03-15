6796e9e6d6f105d665e317564525b89dde979a51
Anosike Osifo <code.anosikeosifo@gmail.com>
2017-03-15 14:08:48 +0100

    T-006 Fix postgresql issue with querying on rails boolean field

--- app/models/event_category.rb
+++ app/models/event_category.rb
@@ -2,6 +2,6 @@ class EventCategory < ApplicationRecord
   has_many :events
 
   def self.for_home_view
-    includes(:events).where('is_active IS 1 AND is_featured IS 1 AND events.rating >= 3').references(:events)
+    includes(:events).where('is_active =? AND is_featured = ? AND events.rating >= 3', true, true).references(:events)
   end
 end
